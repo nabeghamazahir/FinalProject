4862881341c5730b2106ebde60b6495d019241bd
Nabegha Mazahir <nabeghamazahir@outlook.com.au>
2023-06-25 18:38:21 +0930

    Search API updated, results html page updated and test. Mock API cars model dummy data added

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import requests
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
@@ -10,7 +11,22 @@ def welcome():
 def search():
     pickup_date = request.form['pickup']
     dropoff_date = request.form['dropoff']
-    return f'Searching for cars from {pickup_date} to {dropoff_date}'
+
+    response = requests.get('https://64954062b08e17c91791ccc9.mockapi.io/car_models')
+
+    if response.status_code == 200:
+
+        car_models = response.json()
+
+    
+        available_cars = []
+        for car in car_models:
+            if pickup_date in car['available_dates'] and dropoff_date in car['available_dates']:
+                available_cars.append(car)
+
+        return render_template('results.html', cars=available_cars)
+    else:
+        return 'Error occurred while fetching car data'
 
 if __name__ == '__main__':
     app.run()
